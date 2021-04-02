dc9e83d52764f708d31f631b2c1c768fffee0de8
varunbhtt21 <varunbhatt21@gmail.com>
2021-04-02 23:44:23 +0530

    Static input changed

--- Check for palindrome/checkPalindrome.py
+++ Check for palindrome/checkPalindrome.py
@@ -1,23 +1,20 @@
 # Function that checks whether input string is palindrome or not
 def checkPalindrome(inputString):
-    midLength = int(len(inputString) / 2 )           # Finds Mid Length Of String
-    totalLength = int(len(inputString))              # Total Length of String
+    midLength = int(len(inputString) / 2)  # Finds Mid Length Of String
+    totalLength = int(len(inputString))  # Total Length of String
 
     # Compares Beginning Characters with the last characters, Till middle Length
     for index in range(0, midLength):
         if inputString[index] != inputString[totalLength - index - 1]:
             return False
 
-    return True                                  # If All characters Matches , Return True
+    return True  # If All characters Matches , Return True
 
 
 if __name__ == "__main__":
 
-    # 1. Dynamic Input
-    inputString = input("Enter String : ")
-
-    # 2. Static Input :
-    # inputString = "madam"
+    # Static Input :
+    inputString = "madam"
 
     if checkPalindrome(inputString):
         print("Yes")
